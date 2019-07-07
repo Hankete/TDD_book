232d43909c26dcc40f89d12861e92bdbb8f39d9f
Wojtek <wojciech.bialecki1@gmail.com>
2019-07-07 14:07:06 +0200

    App adds up to 1 last item to a list.

--- lists/tests.py
+++ lists/tests.py
@@ -4,10 +4,16 @@ from django.http import HttpRequest
 
 from lists.views import home_page
 
+# Create your tests here.
+
 class HomePageTest(TestCase):
 
-    def test_home_page_return_correct_html(self):
+    def test_home_page_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-# Create your tests here.
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
