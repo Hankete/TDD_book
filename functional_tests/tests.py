b56c4051ec23052b848588c6f115d16c2f05203d
Wojtek <wojciech.bialecki1@gmail.com>
2019-07-14 13:20:58 +0200

    New URL, view and template to display things

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -83,9 +83,11 @@ class NewVisitorTest(LiveServerTestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
+        self.browser.quit()
 
     def test_multiple_users_can_start_lists_at_diffrent_urls(self):
         #Edith starts a new to-do list
+        self.browser = webdriver.Firefox()
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
@@ -103,12 +105,19 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
+        # Francis visits the home page. there is no sign of Edith's
+        # list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
         # Francis starts a new list by entering a new item.
         # He is lesst interesting than Edith
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: buy milk')
+        self.wait_for_row_in_list_table('1: Buy milk')
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
