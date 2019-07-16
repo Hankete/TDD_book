34747f1379a49beac7eaf0afb972f4601f10a8d5
Wojtek <wojciech.bialecki1@gmail.com>
2019-07-16 10:47:59 +0200

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -26,18 +26,29 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    # def test_layout_and_styling(self):
-    #     #Edith goest to a home page
-    #     self.browser.get(self.live_server_url)
-    #     self.browser.set_window_size(1024,768)
+    def test_layout_and_styling(self):
+        #Edith goest to a home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
 
-    #     #She notices the input box is nicely centered
-    #     inputbox = self.browser.find_element_by_id('id_new_item')
-    #     self.assertAlmostEqual(
-    #         inputbox.location['x'] + inputbox.size['width'] / 2,
-    #         512,
-    #         delta=10
-    #         )
+        #She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+            )
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing')
+        inputbpx.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
 
     def test_can_start_a_list_for_one_user(self):
 
