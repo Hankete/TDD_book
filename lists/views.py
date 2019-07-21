5b9f7f180e1f2f81a7cdf5b5d8f6cf06b7c667ba
Wojtek <wojciech.bialecki1@gmail.com>
2019-07-21 17:42:17 +0200

    use new form in home_page, simplify tests. NB breaks stuff

--- lists/views.py
+++ lists/views.py
@@ -8,7 +8,7 @@ from lists.models import Item, List
 # Create your views here.
 def home_page(request):
     items = Item.objects.all()
-    return render(request, "home.html")
+    return render(request, "home.html", {'form': ItemForm()})
 
 
 def view_list(request, list_id):
