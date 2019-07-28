074afede10644852d0a269dfd8fa6c439cc23e19
Wojtek <wojciech.bialecki1@gmail.com>
2019-07-28 15:31:45 +0200

    new lsit view tries to assign owner but can't

--- functional_tests/test_my_list.py
+++ functional_tests/test_my_list.py
@@ -39,7 +39,7 @@ class MyListsTest(FunctionalTest):
         first_list_url = self.browser.current_url
 
         # She notices a "My lists" link, for the first time
-        self.browser.find_element_by_link_text("My list").click()
+        self.browser.find_element_by_link_text("My lists").click()
 
         # She sees that her list is in there, named accordingo to its
         # first list item
